d6b9f70be8816e580e532a9337ce6a6c3ea5cbf5
dianakw8591 <dianakw@gmail.com>
2020-02-03 11:29:06 -0800

    Done.

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,35 @@
 class Employee
+    @@all = []
+    def self.all
+        @@all
+    end
+
+    attr_reader :name
+    attr_accessor :salary, :manager
+    def initialize(name, salary, manager)
+        @name = name
+        @salary = salary
+        @manager = manager
+        self.class.all << self
+    end
+
+    def manager_name
+        self.manager.name
+    end
+
+    def self.paid_over(num)
+        self.all.select {|employee| employee.salary > num}
+    end
+
+    def self.find_by_department(dept_name)
+        self.all.find {|employee| employee.manager.department == dept_name}
+    end
+
+    def tax_bracket
+        bracket = Employee.all.select do |employee|
+            (self.salary - 1000) <= employee.salary && employee.salary <= (self.salary + 1000)
+        end
+        bracket.reject {|employee| employee == self}
+    end
 
 end
--- run.rb
+++ run.rb
@@ -4,7 +4,17 @@ require 'pry'
 
 
 #Test your code here
+bob = Manager.new("Bob", 40, "sales")
+joe = Manager.new("Joe", 30, "shipping")
+cathy = Manager.new("Cathy", 38, "R&D")
+lisa = Manager.new("Lisa", 39, "Design")
 
+
+ben = joe.hire_employee("Ben", 3400)
+diana = cathy.hire_employee("Diana", 2000)
+monica = bob.hire_employee("Monica", 8000)
+ian = lisa.hire_employee("Ian", 2500)
+jess = joe.hire_employee("Jess", 2800)
 
 binding.pry
 puts "done"
