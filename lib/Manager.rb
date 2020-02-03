c32f43f342f2756e55c687eb9623bbacba23c363
github username <github email address>
2020-02-03 13:08:10 -0800

    Done.

--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,41 @@
 class Manager
 
+    attr_reader :name, :department, :age
+
+    @@all = [] 
+
+    def initialize(name, department, age)
+        @name = name 
+        @department = department 
+        @age = age 
+        @employees = []
+        @@all << self
+    end
+
+    def employees
+        @employees 
+    end 
+
+    def employees=(name) 
+        @employees << name
+    end
+
+    def self.all 
+        @@all 
+    end
+
+    def hire_employee(name, salary)
+        employee = Employee.new(name, salary) 
+        employee.manager_name=(self.name)
+        self.employees=(employee)
+    end
+
+    def self.all_departments 
+        self.all.map {|manager| manager.department }.uniq 
+    end
+
+    def self.average_age 
+        self.all.map {|manager| manager.age }.sum / @@all.length 
+    end 
+
 end
